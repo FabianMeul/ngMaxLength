18a7e8fcb626024a72c0decaf179dd2b3957a2c5
Fabian Meul <Fabian.Meul@district01.be>
2016-02-29 16:07:02 +0100

    Initial commit
    * Added bower.json
    * Added source

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # ngMaxLength
 Display the remaining characters of an Angular input field.
+
+
+### Angular Character Limit
+
+This directive extend the existing ng-max-length directive to expose `remainingChars` on the current field scope.
+
+#### Usage
+
+You need to include the ```ng-max-length.js``` file.
+
+
+	// Load the directive
+	<script src="angular.js"></script>
+	<script src="ng-max-length.js"></script>
+	<script src="ng-app.js"></script>
+
+	// In your controller
+	// Include the module in your app
+	var ngMyApp = angular.module('myApp', ['fmNgMaxLength']);
+
+#### Requirements
+
+* AngularJs (Tested against 1.5.0)
+
+#### Todo
+
+* Remove dependency on jQuery
+* Remove assumption on Bootstrap
---
+++ src/ng-max-length.js
@@ -0,0 +1,45 @@
+//
+// Directive: ngMaxlength
+// ---
+// This directive indicates the remaining amount of characters when an ng-maxlength attribute is present
+
+angular.module('fmNgMaxLength')
+    .directive('ngMaxlength', function () {
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            link: function ($scope, element, attrs) {
+
+                var modelWatcher;
+
+                function init() {
+
+                    if (!attrs.ngMaxlength) {
+                        // No maxlength is set, abort execution.
+                        return;
+                    }
+
+                    modelWatcher = $scope.$watch('model', function (nv, ov) {
+                        if (nv) {
+                            $scope.remainingChars = parseInt(attrs.ngMaxlength) - nv.length;
+                        } else {
+                            if (ov && ov.length > 1) {
+                                $scope.remainingChars = 0;
+                            } else {
+                                $scope.remainingChars = parseInt(attrs.ngMaxlength);
+                            }
+                        }
+                    });
+                }
+
+                init();
+
+                // $scope events
+                $scope.$on('$destroy', function () {
+                    if (Function.prototype.isPrototypeOf(modelWatcher)) {
+                        modelWatcher();
+                    }
+                });
+            }
+        };
+});
